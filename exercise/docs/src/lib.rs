55af3f5dffb26ff1495de918e2b49610e830fe8e
Sondre Tagestad <stagesta@cisco.com>
2022-10-09 15:30:40 +0200

    closures done

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -4,6 +4,14 @@
 //
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
+/*!
+Pumpkin library description
+
+# Title
+stuff
+![alt text](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+!*/
+
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
@@ -15,8 +23,13 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// Big orange thing
+/// # Recipes
+/// Coming soon!
 pub struct Pumpkin {
+    /// Percentage
     pub roundness: f32,
+    /// 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,12 +37,14 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// Smash it.
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// Relates to [`Pumpkin::orangeness`]
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
